4d0d42761b6bd8f586c832b0aa990871efe153f8
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2022-10-16 20:12:38 -0300

    Added : NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React from "react"
 
 function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className="imgContainer flex justify-center items-center">
+      <img src="/notFound.png" alt="Not found image" />
+    </div>
   )
 }
 
