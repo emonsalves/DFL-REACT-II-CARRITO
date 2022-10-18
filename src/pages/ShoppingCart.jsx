95db9bb229f9a330895fc7ad9ec6a0ec3d4f7a8d
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2022-10-18 11:01:35 -0300

    Fix : New Logic

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,50 +1,53 @@
 import React, { useContext } from "react"
-import ButtonAddRemove from "../components/ButtonAddRemove"
 import MyContext from "../context/MyContext"
+import ButtonAddRemove from "../components/ButtonAddRemove"
 
 function ShoppingCart() {
-  const { cart } = useContext(MyContext)
-
-  const priceTotal = cart.reduce((pastValue, { total }) => pastValue + total, 0)
+  const { cart, cartTotal, changeToClp } = useContext(MyContext)
 
   return (
     <>
-      <section className="FlexContainer pb-24">
-        {cart &&
-          cart
-            .filter((product) => product.quantity > 0)
-            .map((item) => (
-              <div
-                key={`${item.name}-${item.id}`}
-                className="flex justify-between border border-gray-700 m-2"
-              >
-                <div className="flex items-center">
-                  <img src={`${item.img}`} alt="" className="h-[100px]" />
-                  <h1 className="ml-10">{`Pizza Familiar ${
-                    item.name[0].toUpperCase() + item.name.substring(1)
-                  }`}</h1>
-                </div>
-                <div className="flex justify-center items-center ">
-                  <ButtonAddRemove id={item.id} textBtn={"-"} color="red" />
-                  <h2>{`${item.quantity}`}</h2>
-                  <ButtonAddRemove id={item.id} textBtn={"+"} color="green" />
-                  <h2 className="m-5">
-                    {`${item.total.toLocaleString("es-CL", {
-                      style: "currency",
-                      currency: "CLP",
-                    })}`}
-                  </h2>
-                </div>
+      <section className="FlexContainer mt-40 pb-24">
+        {cartTotal !== changeToClp(0) ? (
+          cart.map((item) => (
+            <div
+              key={`${item.name}-${item.id}`}
+              className="flex justify-between border border-gray-700 m-2"
+            >
+              <div className="flex items-center">
+                <img src={`${item.img}`} alt={`Photo pizza ${item.name}`} className="h-[100px]" />
+                <h1 className="ml-10">{`Pizza Familiar ${item.name[0].toUpperCase() + item.name.substring(1)}`}</h1>
               </div>
-            ))}
+              <div className="flex justify-center items-center ">
+                <ButtonAddRemove
+                  id={item.id}
+                  textBtn={" - "}
+                  color="red"
+                  name={item.name}
+                  img={item.img}
+                  price={item.price}
+                />
+                <h2>{`${item.count}`}</h2>
+                <ButtonAddRemove
+                  id={item.id}
+                  textBtn={" + "}
+                  color="green"
+                  name={item.name}
+                  img={item.img}
+                  price={item.price}
+                />
+                <h2 className="m-5">
+                  {`${changeToClp(item.count * item.price)}`}
+                </h2>
+              </div>
+            </div>
+          ))
+        ) : (
+          <h1> Carrito vacio favor de agregar un producto</h1>
+        )}
 
         <div className="flex justify-end mr-28 mt-6">
-          <h1>
-            {`Total = ${priceTotal.toLocaleString("es-CL", {
-              style: "currency",
-              currency: "CLP",
-            })}`}
-          </h1>
+          <h1>{`Total = ${cartTotal}`}</h1>
         </div>
       </section>
     </>
