f5554c0b09ce761fac3131e609a76264687fca14
Enzo Monsalves <ingeniero.monsalves@gmail.com>
2022-10-18 10:58:32 -0300

    Fix : banner page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,28 @@
 import React, { useContext } from "react"
 import MyContext from "../context/MyContext"
 import Card from "../components/Cards/Card"
+import Banner from "../components/Banner"
 function Home() {
   const { data } = useContext(MyContext)
 
   return (
-    <section className="FlexContainer flex flex-wrap justify-center items-center content-center w-full gap-5 overflow-auto flex-row mt-4 mb-20">
-      {data
-        ? data.map((item) => (
-            <Card
-              key={`${item.id}${item.name}${item.price}`}
-              id={item.id}
-              img={item.img}
-              ingredients={item.ingredients}
-              name={item.name.toUpperCase()}
-              price={item.price}
-            />
-          ))
-        : null}
-    </section>
+    <>
+      <Banner />
+      <section className="FlexContainer flex flex-wrap justify-center items-center content-center w-full gap-5 overflow-auto flex-row mt-4 mb-20">
+        {data
+          ? data.map((item) => (
+              <Card
+                key={`${item.id}${item.name}${item.price}`}
+                id={item.id}
+                img={item.img}
+                ingredients={item.ingredients}
+                name={item.name.toUpperCase()}
+                price={item.price}
+              />
+            ))
+          : null}
+      </section>
+    </>
   )
 }
 
